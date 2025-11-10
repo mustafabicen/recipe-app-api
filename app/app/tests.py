bfca45cd9ef1ac7af15d83d346609e23784e242d
Mustafa Bicen <m.bicen@web.de>
2025-11-10 14:38:02 +0100

    Docker + Django eingebaut

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests for the calc module.
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Tests for the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding two numbers together."""
+        res = calc.add(5, 7)
+
+        self.assertEqual(res, 12)
+        
+    def test_subtract_numbers(self):
+        """Test subtracting two numbers."""
+        res = calc.subtract(10, 4)
+
+        self.assertEqual(res, 6)
